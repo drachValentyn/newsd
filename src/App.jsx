be187dfb203c3a3cd0247bb0a3d1948136e81165
valentyn <valentynd91@gmail.com>
2025-11-08 21:13:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header"
+import Main from "./pages/Main/Main"
+
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <div className="container">
+        <Main></Main>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NewsList/NewsList.jsx
@@ -0,0 +1,17 @@
+import Image from "../Image/Image";
+import NewsItem from "../NewsItem/NewsItem";
+import styles from "./styles.module.css"
+
+const NewsList = ({ news }) => {
+  return (
+    <ul className={styles.list}>
+      {
+        news.map(item => {
+          return <NewsItem key={item.id} item={item} />
+        })
+      }
+    </ul>
+  )
+}
+
+export default NewsList;
