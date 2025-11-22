ebd001cf26206a09cced6b6c3ce34336d749445a
valentyn <valentynd91@gmail.com>
2025-11-22 22:34:08 +0200

    add rtk and rtk query

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,6 +1,7 @@
-import { getCategories } from "../../api/apiNews";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { CategoriesApiResponse, IFilters } from "../../interfaces";
+import { IFilters } from "../../interfaces";
+import { useAppDispatch } from "../../store";
+import { useGetCategoriesQuery } from "../../store/services/newsApi";
+import { setFilters } from "../../store/slices/newsSlice";
 import Categories from "../Categories/Categories";
 import { useTheme } from "../context/ThemeContext";
 import Search from "../Search/Search";
@@ -9,12 +10,12 @@ import styles from "./styles.module.css";
 
 interface Props {
   filters: IFilters;
-  changeFilter: (key: string, value: string | number | null) => void;
 }
 
-const NewsFilters = ({ filters, changeFilter }: Props) => {
-  const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(getCategories);
+const NewsFilters = ({ filters }: Props) => {
+  const { data: dataCategories } = useGetCategoriesQuery(null);
   const { isDark } = useTheme();
+  const dispatch = useAppDispatch();
 
   return (
     <div className={styles.filters}>
@@ -23,15 +24,15 @@ const NewsFilters = ({ filters, changeFilter }: Props) => {
           <Categories
             categories={dataCategories.categories}
             selectedCategory={filters.category}
-            setSelectedCategory={(category) => changeFilter("category", category)}
-          />
+            setSelectedCategory={(category) =>
+              dispatch(setFilters({ key: "category", value: category }))} />
         </Slider>
       ) : null}
 
       <Search
         keywords={filters.keywords}
         isDark={isDark}
-        setKeywords={(keywords) => changeFilter("keywords", keywords)}
+        setKeywords={(keywords) => dispatch(setFilters({ key: "keywords", value: keywords }))}
       />
     </div>
   );
